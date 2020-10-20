69cdf20bf7c78bfaaea6a6ca40c965cb8ed40268
adamlup <31212931+adamlup@users.noreply.github.com>
2020-10-20 18:49:31 +0200

    lab1, form implementation

---
+++ NewBrandingStyleWeb/Controllers/CompanyController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NewBrandingStyleWeb.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NewBrandingStyleWeb.Controllers
+{
+    public class CompanyController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(CompanyModel company)
+        {
+            var viewModel = new CompanyAddedViewModel
+            {
+                NumberOfCharInName = company.Name.Length,
+                NumberOfCharsInDescription = company.Description.Length,
+                IsHidden = !company.IsVisible
+            };
+
+            return View("CompanyAdded", viewModel);
+        }
+    }
+}
